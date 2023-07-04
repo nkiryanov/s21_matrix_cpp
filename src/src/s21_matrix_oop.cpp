e275e94ba0fd04b8097d6cbc78d19654daf5ff02
Nikolay Kiryanov <nikolay@kiryanov.ru>
2023-07-04 15:32:23 +0300

    Simple version of coverage report target + test for constructors

--- src/src/s21_matrix_oop.cpp
+++ src/src/s21_matrix_oop.cpp
@@ -6,16 +6,17 @@ namespace s21 {
 
 S21Matrix::S21Matrix(int rows, int columns) : rows_(rows), columns_(columns) {
   if (rows_ <= 0 || columns_ <= 0) {
-    throw std::length_error("Matrix could have only positive rows or columns");
+    throw std::invalid_argument(
+        "Matrix must have only positive rows or columns");
   }
 
   matrix_ = new double[rows_ * columns];
 }
 
 S21Matrix::S21Matrix(int size) : S21Matrix(size, size) {}
-S21Matrix::S21Matrix() : S21Matrix(3) {}
+S21Matrix::S21Matrix() : S21Matrix(4) {}
 
-int S21Matrix::Rows() {return rows_;}
-int S21Matrix::Columns() {return columns_;}
+int S21Matrix::Rows() { return rows_; }
+int S21Matrix::Columns() { return columns_; }
 
 }  // namespace s21
--- src/tests/matrix/tests_constuctors.cpp
+++ src/tests/matrix/tests_constuctors.cpp
@@ -1,9 +1,29 @@
+#include <stdexcept>
+
 #include "gtest/gtest.h"
 #include "s21_matrix_oop.h"
 
-TEST(MatrixConstructors, SquareMatrixConstructorCreateMatrix) {
-    s21::S21Matrix matrix(5);
+TEST(MatrixConstructors, MatrixSizeCreateSquareMatrix) {
+  s21::S21Matrix matrix(5);
 
-    EXPECT_EQ(matrix.Rows(), 5);
-    EXPECT_EQ(matrix.Columns(), 5);
+  EXPECT_EQ(matrix.Rows(), 5);
+  EXPECT_EQ(matrix.Columns(), 5);
+}
+
+TEST(MatrixConstructors, Create4x4MatrixByDefault) {
+  s21::S21Matrix matrix;
+
+  EXPECT_EQ(matrix.Rows(), 4);
+  EXPECT_EQ(matrix.Columns(), 4);
+}
+
+TEST(MatrixConstructors, CreateMatrixWithRowsAndColumnsIfSet) {
+  s21::S21Matrix matrix(3, 2);
+
+  EXPECT_EQ(matrix.Rows(), 3);
+  EXPECT_EQ(matrix.Columns(), 2);
+}
+
+TEST(MatrixConstructors, ExceptionIfRowsOrColumnsNotValid) {
+  EXPECT_THROW(s21::S21Matrix matrix(3, 0), std::invalid_argument);
 }
